d27c3948a94f07cc10c87b0996d7356c81d4156a
MarchukVolodymyr <volodiamachyk@gmail.com>
2026-02-27 18:08:48 +0000

    idk

--- main.py
+++ main.py
@@ -9,31 +9,25 @@ import threading
 
 
 BOT_TOKEN = "8425242719:AAHTAviHj8N0-eraWv_gEknIIAxf099nAak"
-PORT = 10000
+PORT  = 10000
 
-
-
-disp = Dispatcher(bot=Bot)
+disp = Dispatcher()
 app = FastAPI()
-
-
 @disp.message(Command("start"))
 async def startMethod(message: Message):
     await message.answer("Bot active")
 
+
+
+
 @app.get("/")
 async def check():
-    return {"Status : Bot it running"}
+    return {"status":"bot is running"}
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    await disp.start_polling(bot)
+@app.on_event("startup")
+async def startup():
+    bot  = Bot(token=BOT_TOKEN)
+    asyncio.create_task(disp.start_polling(bot))
 
-
-async def runner():
-    asyncio.run(main())
-    
-    
 if __name__ == "__main__":
-    threading.Thread(target=runner).start()
-    uvicorn.run(app,host = "0.0.0.0", port=PORT)
+    uvicorn.run(app,host ="0.0.0.0",port=PORT)
