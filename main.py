115592f58e3bdf62ec3ebeb795f249dc231924b8
MarchukVolodymyr <volodiamachyk@gmail.com>
2026-02-27 17:04:36 +0000

    unpdate logic

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = "8425242719:AAHTAviHj8N0-eraWv_gEknIIAxf099nAak"
+PORT = 8080
+
+
+
+disp = Dispatcher()
+app = FastAPI()
+
+
+@disp.message(Command("start"))
+async def startMethod(message: Message):
+    await message.answer("Bot active")
+
+@app.get("/")
+async def check():
+    return {"Status : Bot it running"}
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    await disp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    result = asyncio.get_event_loop()
+    result.create_task(main())
+    uvicorn.run(app,host="0.0.0.0", port=PORT)
